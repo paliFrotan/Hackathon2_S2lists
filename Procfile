6f3a2db5797c0325648769c9757f948a53f7d49d
paliFrotan <mspaligill@gmail.com>
2026-03-27 00:59:00 +0000

    changes for heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn plan_todo.wsgi
--- env.py
+++ env.py
@@ -1,9 +1,9 @@
 import os
 
 os.environ['DJANGO_SECRET_KEY'] = 'dev-only-change-me'
-os.environ['DJANGO_DEBUG'] = 'True'  # or 'False'
+os.environ['DJANGO_DEBUG'] = 'False'  # or 'False'
 
 if os.environ['DJANGO_DEBUG'] == 'False':
-    os.environ['DJANGO_ALLOWED_HOSTS'] = 'appname.herokuapp.com'
+    os.environ['DJANGO_ALLOWED_HOSTS'] = 's2lists.herokuapp.com'
 else:
     os.environ['DJANGO_ALLOWED_HOSTS'] = '127.0.0.1,localhost'
