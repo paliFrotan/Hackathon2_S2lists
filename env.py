db16897d8956f880c1c9205cf950127294812de5
paliFrotan <mspaligill@gmail.com>
2026-03-27 09:53:54 +0000

    host allow

--- env.py
+++ env.py
@@ -4,6 +4,6 @@ os.environ['DJANGO_SECRET_KEY'] = 'dev-only-change-me'
 os.environ['DJANGO_DEBUG'] = 'False'  # or 'True'
 
 if os.environ['DJANGO_DEBUG'] == 'False':
-    os.environ['DJANGO_ALLOWED_HOSTS'] = 's2lists.herokuapp.com'
+    os.environ['DJANGO_ALLOWED_HOSTS'] = 's2lists-ff197892594c.herokuapp.com'
 else:
     os.environ['DJANGO_ALLOWED_HOSTS'] = '127.0.0.1,localhost'
